a0a79c12720b88c9c76894e1da66ea1a04b5d340
Lili Rodriguez <lilimrod@gmail.com>
2020-03-16 04:36:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,7 @@ end
 def find_the_cheese(foods)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  foods.include?(cheese_types)
+  foods.find {|foods|
+  include?(cheese_types)
+  }
 end
