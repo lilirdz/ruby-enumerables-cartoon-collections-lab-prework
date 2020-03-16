a9a55ef1afac53f7c4e937570f3fbe6fceacbdbe
Lili Rodriguez <lilimrod@gmail.com>
2020-03-16 04:23:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(calls)
-  calls.map{|element|}
+  calls.map{|element| calls.capitalize + '!'}
+  return calls
 end
 
 def long_planeteer_calls# code an argument here
